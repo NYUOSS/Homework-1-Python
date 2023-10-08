5e2fbd4fcb24ea7542d5a5486712bf1e8dfab032
prajawalsinha07 <ps4771@nyu.edu>
2023-10-08 18:59:45 -0400

    hello world

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,5 @@
 from app import index
 
 def test_index():
-    assert index() == "Software Development HW-1!!!!"
+    assert index() == "Hello World, Software Development HW-1!!!!"
     assert 2+2 ==4
